5dd9119cb8eb3e733a2f0ad069612e5db1625896
Edwin Gamboa <edwjov92@gmail.com>
2016-10-25 09:48:07 -0500

    [CUBBLES-8] Load only manifest JSON (.../manifest.webpackage) and not the whole webpackage manifest

--- webpackages/com.incowia.cubx-webpackage-viewer/cubx-webpackage-viewer/jsFunctions.js
+++ webpackages/com.incowia.cubx-webpackage-viewer/cubx-webpackage-viewer/jsFunctions.js
@@ -8,8 +8,20 @@
   window.com_incowia_cubxWebpackageViewer_cubxWebpackageViewer = {
 
     // Hook function to build the config object to be send to ajaxRequest
-    buildConfigObject: function (url, next) {
+    buildRequestConfigObject: function (url, next) {
       next({url: url});
+    },
+
+    // Hook function to check whether the manifest url is complete or not
+    checkManifestUrl: function (url, next) {
+      var self = window.com_incowia_cubxWebpackageViewer_cubxWebpackageViewer;
+      if (url.indexOf('/manifest.webpackage') < 0) {
+        if(url.lastIndexOf('/') !== url.length - 1) {
+          url += '/';
+        }
+        url += 'manifest.webpackage';
+      }
+      self.buildRequestConfigObject(url, next);
     },
 
     // Hook function to parser the status of a request as boolean
