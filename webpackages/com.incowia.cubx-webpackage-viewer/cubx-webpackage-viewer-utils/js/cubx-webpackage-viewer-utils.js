b2e23c8a0d2cad32d67851d41693af50c449c134
Edwin Gamboa <edwjov92@gmail.com>
2017-04-12 10:39:43 -0500

    CUBBLES-52: Add a utility to have 'global' util functions

---
+++ webpackages/com.incowia.cubx-webpackage-viewer/cubx-webpackage-viewer-utils/js/cubx-webpackage-viewer-utils.js
@@ -0,0 +1,21 @@
+/* globals location */
+'use strict';
+/**
+ * Read url get parameters, similar to PHP.
+ * Source: https://www.creativejuiz.fr/blog/en/javascript-en/read-url-get-parameters-with-javascript
+ * @param {string} param name of the parameter to read
+ * @returns {*} the value of the parameter or an empty object if the parameter was not in the url
+ */
+function $_GET (param) {
+  var vars = {};
+  window.location.href.replace(location.hash, '').replace(
+    /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+    function (m, key, value) { // callback
+      vars[key] = value !== undefined ? value : '';
+    }
+  );
+  if (param) {
+    return vars[param] ? vars[param] : null;
+  }
+  return vars;
+};
