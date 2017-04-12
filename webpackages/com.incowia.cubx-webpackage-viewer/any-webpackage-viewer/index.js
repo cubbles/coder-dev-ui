27a472dd797f2cd47aa0683fe7c072a651acef9f
Edwin Gamboa <edwjov92@gmail.com>
2017-04-12 10:40:53 -0500

    CUBBLES-52: Use 'cubx-webpackage-viewer-utils' in any-webpackage-viewer

--- webpackages/com.incowia.cubx-webpackage-viewer/any-webpackage-viewer/index.js
+++ webpackages/com.incowia.cubx-webpackage-viewer/any-webpackage-viewer/index.js
@@ -1,6 +1,4 @@
-/**
- * Created by Edwin Gamboa on 20/10/2016.
- */
+/* globals $_GET */
 (function () {
   'use strict';
 
@@ -10,7 +8,7 @@
   var alertDText = document.querySelector('#alertDText');
   var cubxWebpackageV = document.querySelector('cubx-webpackage-viewer');
 
-  //When cif is ready the enable load button
+  // When cif is ready the enable load button
   document.addEventListener('cifReady', function () {
     loadB.removeAttribute('disabled');
     var manifestUrlPar = $_GET('manifest-url');
@@ -20,7 +18,7 @@
     }
   });
 
-  //Add event listener to the load button
+  // Add event listener to the load button
   loadB.addEventListener('click', function () {
     if (urlTF.value !== '') {
       cubxWebpackageV.setManifestUrl(urlTF.value);
@@ -29,24 +27,4 @@
       alertD.style.display = 'block';
     }
   });
-
-  /**
-   * Read url get parameters, similar to PHP.
-   * Source: https://www.creativejuiz.fr/blog/en/javascript-en/read-url-get-parameters-with-javascript
-   * @param {string} param name of the parameter to read
-   * @returns {*} the value of the parameter or an empty object if the parameter was not in the url
-   */
-  var $_GET = function (param) {
-    var vars = {};
-    window.location.href.replace( location.hash, '' ).replace(
-      /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
-      function( m, key, value ) { // callback
-        vars[key] = value !== undefined ? value : '';
-      }
-    );
-    if ( param ) {
-      return vars[param] ? vars[param] : null;
-    }
-    return vars; };
-
 }());
