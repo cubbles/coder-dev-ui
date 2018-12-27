76b0e19a1ba1390aaa4ac95d24cb71471d3e09bf
edwingamboa <edwingamboa355@gmail.com>
2018-12-27 10:44:25 -0500

    #6 Update the ubx-manifest-def-extractor

--- webpackages/com.incowia.cubx-webpackage-viewer/cubx-manifest-defs-extractor/cubx-manifest-defs-extractor.js
+++ webpackages/com.incowia.cubx-webpackage-viewer/cubx-manifest-defs-extractor/cubx-manifest-defs-extractor.js
@@ -1,37 +1,8 @@
 (function () {
   'use strict';
-  /**
-   * Get help:
-   * > Lifecycle callbacks:
-   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
-   *
-   */
-  CubxPolymer({
+
+  CubxComponent({
     is: 'cubx-manifest-defs-extractor',
-
-    /**
-     * Manipulate an element’s local DOM when the element is created.
-     */
-    created: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the element is created and initialized.
-     */
-    ready: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the element is attached to the document.
-     */
-    attached: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
-     */
-    cubxReady: function () {
-    },
 
     /**
      *  Observe the Cubbles-Component-Model: If value for slot 'componentArtifactId' has changed ...
