09b599bbb2be9dfdb9317d343eb2537c455c3e67
edwingamboa <edwingamboa355@gmail.com>
2018-12-27 10:39:00 -0500

    #6 Update the cub-structure-viewer

--- webpackages/com.incowia.cubx-webpackage-viewer/cubx-structure-viewer/cubx-structure-viewer.js
+++ webpackages/com.incowia.cubx-webpackage-viewer/cubx-structure-viewer/cubx-structure-viewer.js
@@ -1,16 +1,8 @@
 /* global $, JSONEditor */
 (function () {
   'use strict';
-  /**
-   * Get help:
-   * > Lifecycle callbacks:
-   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
-   *
-   * Access the Cubbles-Component-Model:
-   * > Access slot values:
-   * slot 'a': this.getA(); | this.setA(value)
-   */
-  CubxPolymer({
+
+  CubxComponent({
     is: 'cubx-structure-viewer',
 
     structureHolderId: 'structureViewHolder',
@@ -18,27 +10,9 @@
     depTreeModalId: 'dep_tree_view_modal',
 
     /**
-     * Manipulate an element’s local DOM when the element is created.
-     */
-    created: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the element is created and initialized.
-     */
-    ready: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the element is attached to the document.
-     */
-    attached: function () {
-    },
-
-    /**
      * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
      */
-    cubxReady: function () {
+    contextReady: function () {
       this._addListenerToHideElementsButton();
     },
 
